d968341981f262a97f4cce47165a46e55aaec68a
adam-conway <adam.n.conway@gmail.com>
2018-06-26 17:22:24 -0600

    Removed trailing backslash

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "https://salty-reef-31480.herokuapp.com/";
+	    return "https://salty-reef-31480.herokuapp.com";
 	  }
 	};
 
